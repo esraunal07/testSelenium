a43d0bd3784f89f837704a5a0edeedb97b227592
Esra Unal <gesraunal@gmail.com>
2023-02-18 23:39:55 +0100

    18.02

--- src/test/java/TeamWorkDay_04/Q2_Exercise.java
+++ src/test/java/TeamWorkDay_04/Q2_Exercise.java
@@ -37,7 +37,7 @@ public class Q2_Exercise {
     }
 
     @Test
-    public  void test01(){
+    public void test01() {
         //2. Navigate to url 'http://automationexercise.com'
         driver.get("http://automationexercise.com");
         //3. Verify that home page is visible successfully
@@ -47,26 +47,26 @@ public class Q2_Exercise {
     }
 
     @Test
-    public void test02(){
+    public void test02() {
         //4. Scroll down to footer
         //5. Verify text 'SUBSCRIPTION'
-        WebElement footer=driver.findElement(By.xpath("//h2[text()='Subscription']"));
+        WebElement footer = driver.findElement(By.xpath("//h2[text()='Subscription']"));
         Assert.assertTrue(footer.isDisplayed());
     }
 
     @Test
-    public void test03(){
+    public void test03() {
         //6. Enter email address in input and click arrow button
 
-        driver.findElement(By.id("susbscribe_email")).sendKeys("fidanparlak@gmail.com");
+        driver.findElement(By.id("susbscribe_email")).sendKeys("gesraunal@gmail.com");
         driver.findElement(By.id("subscribe")).click();
         //7. Verify success message 'You have been successfully subscribed!' is visible
-        WebElement message=driver.findElement(By.xpath("//div[text()='You have been successfully subscribed!']"));
+        WebElement message = driver.findElement(By.xpath("//div[text()='You have been successfully subscribed!']"));
 
-        String expectedMessage="You have been successfully subscribed!";
-        String actulalMessage=message.getText();
+        String expectedMessage = "You have been successfully subscribed!";
+        String actulalMessage = message.getText();
 
-        Assert.assertEquals(expectedMessage,actulalMessage);
+        Assert.assertEquals(expectedMessage, actulalMessage);
 
     }
 
@@ -77,17 +77,18 @@ public class Q2_Exercise {
         // 9. cilan sonuc bilgisini bize getirin
         // driveri kapatin
         driver.get("https://www.youtube.com");
-        driver.findElement(By.xpath("/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[2]/ytd-searchbox/form/div[1]/div[1]/input")).sendKeys("Ahmet Bulutluoz"+ Keys.ENTER);
+        driver.findElement(By.xpath("/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[2]/ytd-searchbox/form/div[1]/div[1]/input")).sendKeys("Ahmet Bulutluoz" + Keys.ENTER);
 
-        WebElement aboneSayisi= driver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-channel-renderer/div/div[2]/a/div[1]/div/span[3]"));
+        WebElement aboneSayisi = driver.findElement(By.xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-channel-renderer/div/div[2]/a/div[1]/div/span[3]"));
         //aboneSayisi.getText();
-        String uyeSayisiStr= aboneSayisi.getText();
-        int uyeSayisiIInt=Integer.parseInt(uyeSayisiStr);
-        int expecteedInt=8000;
+        String uyeSayisiStr = aboneSayisi.getText();
+        int uyeSayisiIInt = Integer.parseInt(uyeSayisiStr);
+        int expecteedInt = 8000;
 
-        Assert.assertFalse(uyeSayisiIInt<expecteedInt);
+        Assert.assertFalse(uyeSayisiIInt < expecteedInt);
 
     }
+
 
 
 }
